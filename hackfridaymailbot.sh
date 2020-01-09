f4ba57ca3f2bd54433bf4a9e465c5ba57904e202
Sebastian Steuer <iggy@zxzy.de>
2020-01-09 04:11:05 +0100

    initial commit of the bot

---
+++ hackfridaymailbot.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# muccc hackfriday mail bot
+# hacked together by iggy@muc.ccc.de
+
+# scrape the wiki - find the friday - get the content - send an email
+
+HACKFRIDAY_URL=http://wiki.muc.ccc.de/hackfriday
+EMAIL_ADDRESS=iggy@muc.ccc.de
+
+echo "Scraping $HACKFRIDAY_URL"
+
+DATE_NEXT_FRIDAY=$(date -dnext-friday +%Y-%m-%d)
+DATE_NEXT_NEXT_FRIDAY=$(date -d'next-friday+7 days' +%Y-%m-%d)
+
+echo "Next friday: $DATE_NEXT_NEXT_FRIDAY"
+echo "Next next friday: $DATE_NEXT_NEXT_FRIDAY"
+
+HTML=$(curl -sS $HACKFRIDAY_URL)
+ 
+LINE_FROM=$(grep <<<"$HTML" -n "<h2.*${DATE_NEXT_FRIDAY}</h2>" | cut -f1 -d:)
+LINE_TO=$(grep <<<"$HTML" -n "<h2.*${DATE_NEXT_NEXT_FRIDAY}</h2>" | cut -f1 -d:)
+((LINE_FROM++))
+((LINE_TO--))
+
+echo "Relevant content in line $LINE_FROM to line $LINE_TO"
+
+FRIDAY_HTML=$(sed -n "${LINE_FROM},${LINE_TO}p" <<<"$HTML")
+FRIDAY_TEXT=$(sed -e 's/<[^>]*>//g' <<<"$FRIDAY_HTML" | sed '/^[[:space:]]*$/d')
+
+
+EMAIL_SUBJECT="Hackfriday am ${DATE_NEXT_FRIDAY}"
+
+EMAIL_BODY=$(cat <<EOF
+Werte Lebensformen,
+
+Wir laden ein zum Hackfriday am ${DATE_NEXT_FRIDAY} mit folgendem Programm:
+
+${FRIDAY_TEXT}
+
+Einlass ab 19:00, Programm ab 20:00, anschließend geselliges Beisammensein. 
+
+Alle Angaben ohne Gewähr.
+
+Dieses Schreiben wurde maschinell erstellt und ist ohne Unterschrift gültig.
+EOF
+)
+
+
+echo
+echo "Mail Subject: $EMAIL_SUBJECT"
+echo
+echo "Mail Body:"
+echo "*******"
+echo "$EMAIL_BODY"
+echo "*******"
+
+echo "sending email to $EMAIL_ADDRESS"
+
+mail -s "$EMAIL_SUBJECT" "$EMAIL_ADDRESS" <<<"$EMAIL_BODY" 
+
